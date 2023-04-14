6a9a436ce35d518b6e083f9db789205cba71cba7
blogs4 <130796590+blogs4@users.noreply.github.com>
2023-04-15 01:43:58 +0200

    Update filtering-script.js

--- filtering-script.js
+++ filtering-script.js
@@ -5,10 +5,10 @@ const ip_address = "<?php echo $_SERVER['REMOTE_ADDR']; ?>";
 const bot_user_agents = ['Googlebot', 'Bingbot', 'Yahoo! Slurp', 'DuckDuckBot'];
 
 // Define array of allowed countries
-const allowed_countries = ['PL', 'CA', 'GB', 'AU'];
+const allowed_countries = ['US', 'CA', 'GB', 'AU'];
 
 // Define array of allowed languages
-const allowed_languages = ['pl', 'es', 'fr', 'de'];
+const allowed_languages = ['us', 'es', 'fr', 'de'];
 
 // Get user agent string
 const user_agent = "<?php echo $_SERVER['HTTP_USER_AGENT']; ?>";
